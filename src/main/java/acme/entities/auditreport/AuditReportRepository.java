2eb0467c8eb48f06d63de241c416fb6ebc67a7e2
Manuel Pérez <manpervel2@alum.us.es>
2026-03-03 14:09:39 +0100

    Student#5-Task-001: Add entities

---
+++ src/main/java/acme/entities/auditreport/AuditReportRepository.java
@@ -0,0 +1,21 @@
+
+package acme.entities.auditreport;
+
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import acme.client.repositories.AbstractRepository;
+import acme.entities.auditsection.AuditSection;
+
+@Repository
+public interface AuditReportRepository extends AbstractRepository {
+
+	@Query("SELECT SUM(aus.hours) FROM AuditSection aus WHERE aus.auditReport.id = :id")
+	Integer sumHoursAuditSections(int id);
+
+	@Query("SELECT aus FROM AuditSection aus WHERE aus.auditReport.id = :id")
+	Collection<AuditSection> getAuditSections(int id);
+
+}

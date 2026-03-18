2b3f567023a6d057dc5fc4ecbe19ce569651ddae
Manuel Pérez <manpervel2@alum.us.es>
2026-03-18 12:46:30 +0100

    Student#5-Task-003: add backend to let users acquire/update auditor role

---
+++ src/main/java/acme/features/authenticated/auditor/AuthenticatedAuditorUpdateService.java
@@ -0,0 +1,71 @@
+
+package acme.features.authenticated.auditor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.client.components.principals.Authenticated;
+import acme.client.components.views.SelectChoices;
+import acme.client.services.AbstractService;
+import acme.realms.auditor.Auditor;
+
+@Service
+public class AuthenticatedAuditorUpdateService extends AbstractService<Authenticated, Auditor> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	private AuthenticatedAuditorRepository	repository;
+
+	private Auditor							auditor;
+
+	// AbstractService interface ---------------------------------------------
+
+
+	@Override
+	public void load() {
+		int userAccountId = super.getRequest().getPrincipal().getAccountId();
+
+		this.auditor = this.repository.findAuditorByUserAccountId(userAccountId);
+	}
+
+	@Override
+	public void authorise() {
+		boolean status = super.getRequest().getPrincipal().hasRealmOfType(Auditor.class);
+
+		super.setAuthorised(status);
+	}
+
+	@Override
+	public void bind() {
+		super.bindObject(this.auditor, "firm", "highlights", "solicitor");
+	}
+
+	@Override
+	public void validate() {
+		super.validateObject(this.auditor);
+	}
+
+	@Override
+	public void execute() {
+		this.repository.save(this.auditor);
+	}
+
+	private SelectChoices getsolicitorChoices(final Boolean selected) {
+		SelectChoices result = new SelectChoices();
+
+		result.add("true", "authenticated.auditor.form.value.true", Boolean.TRUE.equals(selected));
+		result.add("false", "authenticated.auditor.form.value.false", Boolean.FALSE.equals(selected));
+
+		return result;
+	}
+
+	@Override
+	public void unbind() {
+		SelectChoices solicitorChoices = this.getsolicitorChoices(this.auditor.getSolicitor());
+
+		super.unbindObject(this.auditor, "firm", "highlights", "solicitor");
+		super.getResponse().addGlobal("solicitorChoices", solicitorChoices);
+	}
+
+}
